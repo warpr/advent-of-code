e98754be9e01fa7d0ae3d62b54a889c490e7d518
Kuno Woudt <kuno@frob.nl>
2023-12-01 19:59:50 -0500

    Day 0x01, part 2 is wrong

---
+++ 2023/day-0x01.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/common.php';
+
+function word_to_digit(array $matches)
+{
+    $word = array_pop($matches);
+    $mapping = array_flip([
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    ]);
+
+    return $mapping[$word] . $word;
+}
+
+function replace_digits(string $line)
+{
+    return preg_replace_callback(
+        '/(one|two|three|four|five|six|seven|eight|nine|zero)/',
+        'word_to_digit',
+        $line
+    );
+}
+
+function parse(string $filename, bool $verbose, bool $part2)
+{
+    $lines = file($filename);
+
+    $ret = [];
+    foreach ($lines as $line) {
+        $before = trim($line);
+        if ($part2) {
+            $line = replace_digits(trim($line));
+        }
+        preg_match('/^[^0-9]*([0-9])/', $line, $matches);
+        $first = $matches[1];
+        preg_match("/.*([0-9])[^0-9]*$/", $line, $matches);
+        $last = $matches[1];
+        $ret[] = (int) "{$first}{$last}";
+
+        if ($verbose) {
+            echo "\n";
+            echo "before: $before\n";
+            echo "after:  $line\n";
+            echo "digits: {$first}{$last}\n";
+        }
+    }
+
+    return $ret;
+}
+
+function main(string $filename, bool $verbose, bool $part2)
+{
+    $values = parse($filename, $verbose, $part2);
+
+    if ($verbose) {
+        print_r(compact('values'));
+    }
+
+    return array_sum($values);
+}
+
+function part1(string $filename, bool $verbose)
+{
+    return main($filename, $verbose, false);
+}
+
+function part2(string $filename, bool $verbose)
+{
+    return main($filename, $verbose, true);
+}
+
+run_part1('example', true, 142);
+run_part1('input', false);
+echo "\n";
+
+run_part2('example2', true, 281);
+run_part2('input', false);
+echo "\n";
