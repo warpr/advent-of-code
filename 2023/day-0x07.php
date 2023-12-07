4a7e761c8d8e0e4150de587b3e5e776355082464
Kuno Woudt <kuno@frob.nl>
2023-12-07 04:36:43 -0500

    Day 0x07, part 1

--- 2023/day-0x07.php
+++ 2023/day-0x07.php
@@ -4,115 +4,86 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/common.php';
 
-$strength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
-
-$strength = [
-    '2' => 2,
-    '3' => 3,
-    '4' => 4,
-    '5' => 5,
-    '6' => 6,
-    '7' => 7,
-    '8' => 8,
-    '9' => 9,
-    'T' => 10,
-    'J' => 11,
-    'Q' => 12,
-    'K' => 13,
-    'A' => 14,
-];
-
-/*
-    Five of a kind, where all five cards have the same label: AAAAA
-    Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-    Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-    Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-    Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-    One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-    High card, where all cards' labels are distinct: 23456
- */
-
-function analyze_race(bool $verbose, int $time, int $distance)
+function hand_type(string $hand)
 {
-    $total_races = $time;
-    $losing_start = 0;
-    $losing_end = 0;
+    $count = [];
 
-    for ($held = 0; $held <= $time; $held++) {
-        $speed = $held;
-        $left = $time - $held;
-        $run = $speed * $left;
-
-        if ($run > $distance) {
-            $losing_start = $held;
-            break;
-        }
+    $parts = str_split($hand);
+    foreach ($parts as $card) {
+        @$count[$card]++;
     }
 
-    for ($held = $time; $held > 0; $held--) {
-        $speed = $held;
-        $left = $time - $held;
-        $run = $speed * $left;
+    $count = array_values($count);
+    rsort($count);
 
-        if ($run > $distance) {
-            $losing_end = $time - $held - 1;
-            break;
-        }
+    // this will sort correctly as strings, e.g. full
+    // house is represented as "3-2", one pair as
+    // "2-1-1-1", etc..
+    return implode('-', $count);
+}
+
+function cmp_hands(string $a, string $b)
+{
+    $on_type = strcmp(hand_type($a), hand_type($b));
+    if ($on_type) {
+        return $on_type;
     }
 
-    return $total_races - $losing_start - $losing_end;
+    return strcmp($a, $b);
 }
 
 function parse(string $filename, bool $verbose, bool $part2)
 {
     $lines = file($filename);
 
-    $input = [];
+    $hands = [];
+
     foreach ($lines as $line) {
         $line = trim($line);
         if (empty($line)) {
             continue;
         }
 
-        list($field, $values) = explode(':', $line);
-        $input[$field] = array_values(array_filter(explode(' ', trim($values))));
+        list($hand, $bid) = explode(' ', $line);
+
+        // replace card names to make them
+        // string sortable.
+        $hand = str_replace(['T', 'J', 'Q', 'K', 'A'], ['B', 'C', 'D', 'E', 'F'], $hand);
+        $hands[$hand] = $bid;
     }
 
-    $values = [];
-
-    if ($part2) {
-        $time = (int) implode('', $input['Time']);
-        $distance = (int) implode('', $input['Distance']);
-        return analyze_race($verbose, (int) $time, (int) $distance);
+    uksort($hands, 'cmp_hands');
+    if ($verbose) {
+        print_r(['sorted' => $hands]);
     }
 
-    foreach ($input['Time'] as $race_no => $time) {
-        $distance = $input['Distance'][$race_no];
-        $values[$race_no] = analyze_race($verbose, (int) $time, (int) $distance);
+    $ret = [];
+
+    $rank = 1;
+    foreach ($hands as $hand => $bid) {
+        $ret[] = $rank * $bid;
+        $rank++;
     }
 
-    return $values;
+    return $ret;
 }
 
 function main(string $filename, bool $verbose, bool $part2)
 {
-    if ($part2) {
-        return parse($filename, $verbose, $part2);
-    }
-
     $values = parse($filename, $verbose, $part2);
 
     if ($verbose) {
         print_r(compact('values'));
     }
 
-    return array_product($values);
+    return array_sum($values);
 }
 
-run_part1('example', true, 288);
+run_part1('example', true, 6440);
 run_part1('input', false);
 echo "\n";
-
+/*
 run_part2('example', true, 71503);
 run_part2('input', false);
 echo "\n";
+*/
