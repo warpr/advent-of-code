d97a812a15e19cd90eb1b472c5fc2b0e4345d579
Kuno Woudt <kuno@frob.nl>
2023-12-09 04:24:22 -0500

    Day 0x09, part 2

--- 2023/day-0x09.php
+++ 2023/day-0x09.php
@@ -47,6 +47,27 @@ function process_history(bool $verbose, array $history)
     return array_pop($result);
 }
 
+function process_history_part2(bool $verbose, array $history)
+{
+    $log = [$history];
+
+    $tmp = $history;
+    do {
+        $tmp = array_map(fn($i) => $i[1] - $i[0], iterator_to_array(pairs($tmp)));
+        $log[] = $tmp;
+    } while (!is_all_zeroes($tmp));
+
+    $log = array_reverse($log);
+    $last_item = 0;
+    foreach ($log as $idx => $record) {
+        $last_item = $record[0] - $last_item;
+        $log[$idx] = array_merge([$last_item], $record);
+    }
+
+    $result = array_pop($log);
+    return array_shift($result);
+}
+
 function parse(string $filename, bool $verbose, bool $part2)
 {
     $lines = file($filename);
@@ -58,7 +79,11 @@ function parse(string $filename, bool $verbose, bool $part2)
             continue;
         }
 
-        $ret[] = process_history($verbose, $history);
+        if ($part2) {
+            $ret[] = process_history_part2($verbose, $history);
+        } else {
+            $ret[] = process_history($verbose, $history);
+        }
     }
 
     return $ret;
@@ -72,16 +97,15 @@ function main(string $filename, bool $verbose, bool $part2)
         print_r(compact('values'));
     }
 
-    // 18, 28, 68
+    // 5 -3 0
 
     return array_sum($values);
 }
 
-run_part1('example', true, 114);
+run_part1('example', false, 114);
 run_part1('input', false);
 echo "\n";
-/*
-run_part2('example3', true, 6);
+
+run_part2('example', true, 2);
 run_part2('input', false);
 echo "\n";
-*/
