f7e165424bd0ebc13b553cc43082faaebd175d9c
Kuno Woudt <kuno@frob.nl>
2023-12-09 04:16:06 -0500

    Day 0x09, part 1

--- 2023/day-0x09.php
+++ 2023/day-0x09.php
@@ -4,111 +4,84 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/common.php';
 
-function part1(array $instructions, array $paths)
+function pairs(array $arr)
 {
-    $steps = 0;
-    $current = 'AAA';
-    while (true) {
-        foreach ($instructions as $cmd) {
-            $steps++;
-            $current = $paths[$current][$cmd] ?? null;
-            if (empty($current)) {
-                echo "Game stopped\n";
-                print_r(compact('current', 'cmd'));
-                die();
-            }
-            if ($current === 'ZZZ') {
-                return $steps;
-            }
-        }
+    if (count($arr) < 2) {
+        return [];
     }
 
-    return null;
+    for ($i = 1; $i < count($arr); $i++) {
+        yield [$arr[$i - 1], $arr[$i]];
+    }
 }
 
-function part2(array $instructions, array $paths)
+function is_all_zeroes(array $arr)
 {
-    $steps = 0;
-    $current = array_values(array_filter(array_keys($paths), fn($i) => str_ends_with($i, 'A')));
-
-    $steps_since_z = [];
-    $z_steps = [];
-
-    while (true) {
-        foreach ($instructions as $cmd) {
-            $steps++;
-            foreach ($current as $idx => $here) {
-                $current[$idx] = $paths[$here][$cmd] ?? null;
-                @$steps_since_z[$idx]++;
-            }
-
-            $non_zees = array_filter($current, fn($i) => !str_ends_with($i, 'Z'));
-            if (empty($non_zees)) {
-                return $steps;
-            }
-
-            foreach ($current as $idx => $node) {
-                if (str_ends_with($node, 'Z')) {
-                    $z_steps[$idx] = $steps_since_z[$idx];
-                    $steps_since_z[$idx] = 0;
-                }
-            }
-
-            // give it some random amount of iterations
-            if ($steps > 100000) {
-                break 2;
-            }
+    foreach ($arr as $item) {
+        if ($item !== 0) {
+            return false;
         }
     }
 
-    $common = array_shift($z_steps);
-    while (count($z_steps)) {
-        $common = gmp_lcm($common, array_shift($z_steps));
+    return true;
+}
+
+function process_history(bool $verbose, array $history)
+{
+    $log = [$history];
+
+    $tmp = $history;
+    do {
+        $tmp = array_map(fn($i) => $i[1] - $i[0], iterator_to_array(pairs($tmp)));
+        $log[] = $tmp;
+    } while (!is_all_zeroes($tmp));
+
+    $log = array_reverse($log);
+    $last_item = 0;
+    foreach ($log as $idx => $record) {
+        $last_item = end($record) + $last_item;
+        $log[$idx][] = $last_item;
     }
 
-    return $common;
+    $result = array_pop($log);
+    return array_pop($result);
 }
 
 function parse(string $filename, bool $verbose, bool $part2)
 {
     $lines = file($filename);
 
-    $instructions = str_split(trim(array_shift($lines)));
-    $paths = [];
-
+    $ret = [];
     foreach ($lines as $line) {
-        $line = trim($line);
-        if (empty($line)) {
+        $history = array_map(fn($i) => (int) trim($i), explode(' ', $line));
+        if (empty($history)) {
             continue;
         }
 
-        if (preg_match('/(.*) = \((.*), (.*)\)/', $line, $matches)) {
-            $paths[$matches[1]] = [
-                'L' => $matches[2],
-                'R' => $matches[3],
-            ];
-        }
+        $ret[] = process_history($verbose, $history);
     }
 
-    return [$instructions, $paths];
+    return $ret;
 }
 
 function main(string $filename, bool $verbose, bool $part2)
 {
-    list($instructions, $paths) = parse($filename, $verbose, $part2);
+    $values = parse($filename, $verbose, $part2);
 
-    if ($part2) {
-        return part2($instructions, $paths);
+    if ($verbose) {
+        print_r(compact('values'));
     }
 
-    return part1($instructions, $paths);
+    // 18, 28, 68
+
+    return array_sum($values);
 }
 
-run_part1('example', true, 2);
-run_part1('example2', true, 6);
+run_part1('example', true, 114);
 run_part1('input', false);
 echo "\n";
-
+/*
 run_part2('example3', true, 6);
 run_part2('input', false);
 echo "\n";
+*/
