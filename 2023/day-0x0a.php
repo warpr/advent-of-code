b17f933335291b6f07ea2bceb1d0720181648f82
Kuno Woudt <kuno@frob.nl>
2023-12-10 12:34:56 -0500

    Day 0x0A, part 2

--- 2023/day-0x0a.php
+++ 2023/day-0x0a.php
@@ -4,6 +4,38 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/common.php';
 
+function pipe_mapping()
+{
+    return [
+        '|' => ['N', 'S'],
+        '-' => ['E', 'W'],
+        'L' => ['E', 'N'],
+        'J' => ['N', 'W'],
+        '7' => ['S', 'W'],
+        'F' => ['E', 'S'],
+    ];
+}
+
+function find_direction(array $a, array $b)
+{
+    if ($a[0] === $b[0]) {
+        return $a[1] < $b[1] ? 'S' : 'N';
+    } else {
+        return $a[0] < $b[0] ? 'W' : 'E';
+    }
+}
+
+function patch_grid(array &$grid, array $start, array $start_shape)
+{
+    sort($start_shape);
+
+    foreach (pipe_mapping() as $chr => $shape) {
+        if ($start_shape == $shape) {
+            $grid[$start[1]][$start[0]] = $chr;
+        }
+    }
+}
+
 function invert_dir(string $dir)
 {
     switch ($dir) {
@@ -38,20 +70,13 @@ function follow_pipe(
     string $from,
     string $pipe_override = null
 ) {
+    $pipe_mapping = pipe_mapping();
+
     $directions = [
         'N' => [$current[0], $current[1] - 1],
         'E' => [$current[0] + 1, $current[1]],
         'S' => [$current[0], $current[1] + 1],
         'W' => [$current[0] - 1, $current[1]],
-    ];
-
-    $pipe_mapping = [
-        '|' => ['N', 'S'],
-        '-' => ['E', 'W'],
-        'L' => ['N', 'E'],
-        'J' => ['N', 'W'],
-        '7' => ['W', 'S'],
-        'F' => ['E', 'S'],
     ];
 
     $pipe = $grid[$current[1]][$current[0]];
@@ -76,7 +101,7 @@ function follow_pipe(
     ];
 }
 
-function find_loop(bool $verbose, array $grid, array $start, string $dir, string $pipe_override)
+function find_loop(bool $verbose, array &$grid, array $start, string $dir, string $pipe_override)
 {
     $coords = [$start];
 
@@ -102,6 +127,82 @@ function find_loop(bool $verbose, array $grid, array $start, string $dir, string
     }
 }
 
+function clear_junk(array &$grid, array $path)
+{
+    $hash = [];
+    foreach ($path as $coord) {
+        $key = implode(',', $coord);
+        $hash[$key] = true;
+    }
+
+    foreach ($grid as $y => $row) {
+        foreach (str_split($row) as $x => $chr) {
+            $pos = implode(',', [$x, $y]);
+            if (!array_key_exists($pos, $hash)) {
+                $grid[$y][$x] = '.';
+            }
+        }
+    }
+}
+
+function ray_cast(array &$grid)
+{
+    $pipe_mapping = pipe_mapping();
+
+    $painted = 0;
+    foreach ($grid as $y => $row) {
+        $outside = true;
+
+        $seen = [];
+
+        $debug = $y == 62;
+
+        foreach (str_split($row) as $x => $chr) {
+            $exits = $pipe_mapping[$chr] ?? [];
+
+            if (array_search('N', $exits) !== false) {
+                $seen[] = 'N';
+            }
+
+            if (array_search('S', $exits) !== false) {
+                $seen[] = 'S';
+            }
+
+            if (count($seen) >= 2) {
+                if ($seen[0] !== $seen[1]) {
+                    // crossed N-S or S-N pipe
+                    $outside = !$outside;
+                }
+                $seen = [];
+            }
+
+            if (!$outside && $chr == '.') {
+                $painted++;
+                $grid[$y][$x] = '*';
+            }
+        }
+    }
+
+    return $painted;
+}
+
+function area(bool $verbose, array $grid, array $path)
+{
+    $start = $path[0];
+    $first = $path[1];
+    $last = end($path);
+    $shape = [find_direction($start, $first), find_direction($start, $last)];
+
+    patch_grid($grid, $start, $shape);
+    clear_junk($grid, $path);
+    $painted = ray_cast($grid);
+    if ($verbose) {
+        print_r($grid);
+    }
+
+    return $painted;
+}
+
 function parse(string $filename, bool $verbose, bool $part2)
 {
     $lines = file($filename);
@@ -111,15 +212,11 @@ function parse(string $filename, bool $verbose, bool $part2)
         $grid[] = trim($line);
     }
 
-    if ($verbose) {
-        print_r($grid);
-    }
-
     $start = find_start($grid);
 
     $starting_directions = [
-        'N' => '|',
         'E' => '-',
+        'N' => '|',
         'S' => '|',
         'W' => '-',
     ];
@@ -131,6 +228,14 @@ function parse(string $filename, bool $verbose, bool $part2)
         }
     }
 
+    if ($part2) {
+        return area($verbose, $grid, $path);
+    }
+
+    if ($verbose) {
+        print_r($grid);
+    }
+
     return (int) round(count($path) / 2);
 }
 
@@ -140,11 +245,12 @@ function main(string $filename, bool $verbose, bool $part2)
 }
 
 run_part1('example', true, 4);
-run_part1('example2', false, 8);
+run_part1('example2', true, 8);
 run_part1('input', false);
 echo "\n";
-/*
-run_part2('example', true, 2);
+
+run_part2('example3', true, 4);
+run_part2('example4', true, 8);
+run_part2('example5', true, 10);
 run_part2('input', false);
 echo "\n";
-*/
