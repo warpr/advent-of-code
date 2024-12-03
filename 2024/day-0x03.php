02317f16f11d079932d5ef4299f56bae2e7f92df
Kuno Woudt <kuno@frob.nl>
2024-12-03 05:02:39 -0500

    day 3, part 2

--- 2024/day-0x03.php
+++ 2024/day-0x03.php
@@ -16,21 +16,59 @@ function parse(string $filename, bool $part2)
 {
     $body = file_get_contents($filename);
 
-    if (!preg_match_all('/mul\(([0-9]{1,3}),([0-9]{1,3})\)/', $body, $matches)) {
+    $do = preg_quote('do()');
+    $dont = preg_quote("don't()");
+    $mul = 'mul\([0-9]{1,3},[0-9]{1,3}\)';
+
+    if (!preg_match_all("/($do|$dont|$mul)/", $body, $matches)) {
         die('No valid instructions found');
     }
 
-    return array_map(null, $matches[1], $matches[2]);
+    return $matches[0];
 }
 
 function part1($values)
 {
-    return array_map(fn($i) => $i[0] * $i[1], $values);
+    $ret = [];
+
+    foreach ($values as $instr) {
+        if (preg_match('/mul\(([0-9]+),\s*([0-9]+)\)/', $instr, $matches)) {
+            $ret[] = $matches[1] * $matches[2];
+        }
+    }
+
+    return $ret;
 }
 
 function part2($values)
 {
-    return [23];
+    $ret = [];
+
+    $disabled = false;
+
+    foreach ($values as $instr) {
+        if ($instr == 'do()') {
+            $disabled = false;
+            continue;
+        }
+
+        if ($instr == "don't()") {
+            $disabled = true;
+            continue;
+        }
+
+        if ($disabled) {
+            vecho::msg("skipping $instr");
+            continue;
+        }
+
+        if (preg_match('/mul\(([0-9]+),\s*([0-9]+)\)/', $instr, $matches)) {
+            vecho::msg("calculating $instr");
+            $ret[] = $matches[1] * $matches[2];
+        }
+    }
+
+    return $ret;
 }
 
 function main(string $filename, bool $part2)
@@ -51,10 +89,11 @@ function main(string $filename, bool $part2)
 }
 
 // 161 (2*4 + 5*5 + 11*8 + 8*5).
-run_part1('example', true, 161);
+run_part1('example', false, 161);
 run_part1('input', false);
 echo "\n";
 
-// run_part2('example', false, 4);
-// run_part2('input', false);
-// echo "\n";
+// 48 (2*4 + 8*5)
+run_part2('example2', true, 48);
+run_part2('input', false);
+echo "\n";
