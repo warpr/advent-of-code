93cb091027848880403e4ef50aa9b1a8a52cbef2
Kuno Woudt <kuno@frob.nl>
2024-12-12 04:04:48 -0500

    day 12 inputs

---
+++ 2024/day-0x0c.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *   Copyright (C) 2024  Kuno Woudt <kuno@frob.nl>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of copyleft-next 0.3.1.  See copyleft-next-0.3.1.txt.
+ *
+ *   SPDX-License-Identifier: copyleft-next-0.3.1
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/common.php';
+
+function parse(string $filename, bool $part2)
+{
+    $lines = file($filename);
+
+    $grid = [];
+    foreach ($lines as $line) {
+        $line = trim($line);
+
+        $grid[] = $line;
+    }
+
+    return new grid($grid);
+}
+
+function part1($stones)
+{
+    return [23];
+}
+
+function part2($stones)
+{
+    return [23];
+}
+
+function main(string $filename, bool $part2)
+{
+    $parsed = parse($filename, $part2);
+
+    if ($part2) {
+        $values = part2($parsed);
+    } else {
+        $values = part1($parsed);
+    }
+
+    if (vecho::$verbose) {
+        print_r($values);
+    }
+
+    return array_sum($values);
+}
+
+run_part1('example', false, 1930);
+run_part1('input', false);
+echo "\n";
+
+run_part2('input', true);
+echo "\n";
