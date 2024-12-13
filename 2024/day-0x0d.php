0baeb66efbb4eafe06a2afc810d090a8568681d5
Kuno Woudt <kuno@frob.nl>
2024-12-13 08:56:45 -0500

    day 13 part 2

--- 2024/day-0x0d.php
+++ 2024/day-0x0d.php
@@ -65,6 +65,35 @@ function brute_force($machine) {
     return $answers;
 }
 
+function to_integer(float $val) {
+    $tmp = abs(round($val) - $val);
+    if ($tmp < 0.0001) {
+        return (int) round($val);
+    }
+
+    return null;
+}
+
+function math($machine) {
+    $px = $machine['prize']['x'] + 10000000000000;
+    $py = $machine['prize']['y'] + 10000000000000;
+    $ax = $machine['button-a']['x'];
+    $ay = $machine['button-a']['y'];
+    $bx = $machine['button-b']['x'];
+    $by = $machine['button-b']['y'];
+
+    $b = to_integer(($py - $px * $ay / $ax) / ($by - $ay * $bx / $ax));
+    if ($b === null) {
+        return [];
+    }
+    $a = to_integer($px / $ax - $b * $bx / $ax);
+    if ($a === null) {
+        return [];
+    }
+
+    return [ [ $a, $b ] ];
+}
+
 function cost(array $answers) {
     return $answers[0] * 3 + $answers[1];
 }
@@ -93,7 +122,24 @@ function part1($input)
 
 function part2($input)
 {
-    return [ 23 ];
+    $ret = [];
+
+    foreach ($input as $idx => $machine) {
+        $valid_answers = math($machine);
+        if (empty($valid_answers)) {
+            vecho::msg("machine $idx has no valid answers", compact('machine'));
+            continue;
+        }
+
+        $costs = array_map('cost', $valid_answers);
+        sort($costs);
+        vecho::msg("machine $idx valid answers", compact('machine', 'valid_answers', 'costs'));
+        $ret[] = array_shift($costs);
+    }
+
+    vecho::$verbose = false;
+
+    return $ret;
 }
 
 function main(string $filename, bool $part2)
@@ -117,6 +163,6 @@ run_part1('example', false, 480);
 run_part1('input', false);
 echo "\n";
 
-// run_part2('example', false, 1206);
-// run_part2('input', false);
+run_part2('example', false, 875318608908);
+run_part2('input', false);
 echo "\n";
