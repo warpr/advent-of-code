0ff3b0aac61042a8630009c864335366c86b1a56
Kuno Woudt <kuno@frob.nl>
2024-12-15 22:26:00 -0500

    day 15 part 2 wip

--- 2024/day-0x0f.php
+++ 2024/day-0x0f.php
@@ -83,6 +83,106 @@ function simulate(int $stepno, grid $grid, pos $robot, pos $dir)
     return $robot->add($dir);
 }
 
+function simulate_h(int $stepno, grid $grid, pos $robot, pos $dir)
+{
+    $pos = $robot;
+
+    $current = '@';
+    $found = [(object) ['val' => '@', 'pos' => $pos]];
+    do {
+        $pos = $pos->add($dir);
+        $current = $grid->get($pos);
+        $found[] = (object) ['val' => $current, 'pos' => $pos];
+    } while ($current == '[' || $current == ']');
+
+    $prefix = "$stepno. Seeing [" . implode('', array_column($found, 'val')) . ']';
+
+    $end = array_pop($found);
+    if ($end->val != '.') {
+        vecho::msg($prefix . ', not moving');
+        return $robot;
+    }
+
+    vecho::msg($prefix . ', moving (maybe pushing boxes)');
+
+    $to_move = array_reverse($found);
+    foreach ($to_move as $t) {
+        $grid->set($t->pos->add($dir), $t->val);
+    }
+    $grid->set($robot, '.');
+
+    return $robot->add($dir);
+}
+
+function next_step(grid $grid, array $locations, pos $dir)
+{
+    $next_row = [];
+
+    foreach ($locations as $loc) {
+        $front = $loc->pos->add($dir);
+        $current = $grid->get($front);
+
+        switch ($current) {
+            case '[':
+                $next_row[(string) $front] = (object) ['val' => $current, 'pos' => $front];
+                $pos = $front->add(turn_right($dir));
+                $val = $grid->get($pos);
+                $next_row[(string) $pos] = (object) compact('val', 'pos');
+                break;
+            case ']':
+                $next_row[(string) $front] = (object) ['val' => $current, 'pos' => $front];
+                $pos = $front->add(turn_left($dir));
+                $val = $grid->get($pos);
+                $next_row[(string) $pos] = (object) compact('val', 'pos');
+                break;
+            case '.':
+                break;
+            default:
+                return false;
+        }
+    }
+
+    return $next_row;
+}
+
+function simulate_v(int $stepno, grid $grid, pos $robot, pos $dir)
+{
+    $rows = [];
+
+    $next_row = [(object) ['val' => '@', 'pos' => $robot]];
+    $rows[] = $next_row;
+    while (true) {
+        $next_row = next_step($grid, $next_row, $dir);
+        if ($next_row === false) {
+            return $robot;
+        }
+        if (empty($next_row)) {
+            break;
+        }
+        $rows[] = $next_row;
+    }
+
+    $to_move = array_reverse($rows);
+    foreach ($to_move as $row) {
+        foreach ($row as $t) {
+            $grid->set($t->pos->add($dir), $t->val);
+            $grid->set($t->pos, '.');
+            $grid->render(10);
+        }
+    }
+
+    return $grid->find_first('@');
+}
+
+function simulate2(int $stepno, grid $grid, pos $robot, pos $dir)
+{
+    if (is_horizontal($dir)) {
+        return simulate_h($stepno, $grid, $robot, $dir);
+    } else {
+        return simulate_v($stepno, $grid, $robot, $dir);
+    }
+}
+
 function part1(array $input)
 {
     extract($input);
@@ -106,9 +206,53 @@ function part1(array $input)
     return $gps;
 }
 
+function double_grid(grid $grid): grid
+{
+    $grid2 = [];
+    foreach ($grid->grid as $row) {
+        $line = [];
+        foreach (str_split($row) as $chr) {
+            if ($chr == 'O') {
+                $line[] = '[';
+                $line[] = ']';
+            } elseif ($chr == '@') {
+                $line[] = '@';
+                $line[] = '.';
+            } else {
+                $line[] = $chr;
+                $line[] = $chr;
+            }
+        }
+        $grid2[] = implode('', $line);
+    }
+
+    return new grid($grid2);
+}
+
 function part2(array $input)
 {
-    return [23];
+    extract($input);
+
+    $grid = double_grid($grid);
+    $robot = $grid->find_first('@');
+
+    foreach ($movements as $step => $m) {
+        $robot = simulate2($step, $grid, $robot, $m);
+        $grid->render($step > 180 ? 500 : 1);
+
+        if ($step > 220) {
+            die();
+        }
+    }
+
+    $boxes = $grid->find_all('[');
+
+    $gps = [];
+    foreach ($boxes as $box) {
+        $gps[] = 100 * $box->y + $box->x;
+    }
+
+    return $gps;
 }
 
 function main(string $filename, bool $part2)
@@ -128,11 +272,11 @@ function main(string $filename, bool $part2)
     return array_sum($values);
 }
 
-run_part1('example1', true, 2028);
-run_part1('example', true, 10092);
+run_part1('example1', false, 2028);
+run_part1('example', false, 10092);
 run_part1('input', false);
 echo "\n";
 
-// run_part2('example', true);
+run_part2('example', true, 9021);
 // run_part2('input', true);
 echo "\n";
